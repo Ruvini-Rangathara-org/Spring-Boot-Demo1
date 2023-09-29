f04ad1631a6acc72ffd679f5ecd2030d835e3089
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-09-29 09:42:51 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootDevStack/SpringBootDevStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDevStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevStackApplication.class, args);
+	}
+
+}
