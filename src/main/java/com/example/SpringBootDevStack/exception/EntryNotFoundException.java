4fe8d106a8322b7442a457a88fe8c414e258d691
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-09-30 11:32:57 +0530

    Finalize

--- src/main/java/com/example/SpringBootDevStack/exception/EntryNotFoundException.java
+++ src/main/java/com/example/SpringBootDevStack/exception/EntryNotFoundException.java
@@ -1,8 +1,6 @@
 package com.example.SpringBootDevStack.exception;
 
 public class EntryNotFoundException extends RuntimeException{
-    private String message;
-
     public EntryNotFoundException(String message){
         super(message);
     }
